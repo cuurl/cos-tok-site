40fda7101c42e23ad1e65639f2dd46c335dd18e3
cuurl <james.richardsonn1@gmail.com>
2022-04-12 11:34:22 -0400

    login page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,54 @@
-import * as React from "react"
+import React, { Component } from "react"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
+// Imports Netlify Identity script into HTML of site.
+// Must be called from React life-cycle function. Will not work if called at GatsbyJS build-time.
+
+function initNetlifyIdentity() {
+  console.log("initNetlifyIdentity called")
+  const script = document.createElement("script")
+
+  script.src = "https://identity.netlify.com/v1/netlify-identity-widget.js"
+  script.async = true
+
+  document.body.appendChild(script)
+
+}
+
+function openNetlifyModal() {
+  const netlifyIdentity = window.netlifyIdentity;
+
+  if (netlifyIdentity) 
+    netlifyIdentity.open();
+  else 
+    console.log("netlifyIdentity not defined");
+  
+}
+
+class NetlifyIdentity extends Component {
+  componentDidMount() {
+    initNetlifyIdentity();
+  }
+
+  render() {
+    return (<div></div>);
+  }
+}
+
+const IndexPage = () => {
+  return(
+    <Layout>
+      <NetlifyIdentity />
+      <Seo title="Home" />
+      <h1>Hi people</h1>
+      <h2 onClick={() =>  {openNetlifyModal()}}>Login</h2>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+     <StaticImage
       src="../images/gatsby-astronaut.png"
       width={300}
       quality={95}
@@ -27,5 +64,6 @@ const IndexPage = () => (
     </p>
   </Layout>
 )
+ }
 
 export default IndexPage
